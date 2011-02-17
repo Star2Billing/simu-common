693a5359e03c5aa5448dda0dfe5772e11ccf7a4a
Areski Belaid <areski@gmail.com>
2011-02-17 14:26:07 +0100

    Import project

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='simu-common',
+      description='Django Common Libs',
+      author='Belaid Arezqui',
+      author_email='areski@gmail.com',
+      packages=['simu-common'],
+     )
